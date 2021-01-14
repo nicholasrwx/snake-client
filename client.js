f4237b6cfc7bf755f73ea60f3773e22fae7f8952
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-01-14 03:37:18 +0000

    new module for stdin

--- client.js
+++ client.js
@@ -1,28 +1,5 @@
 const net = require("net");
 
-
-
-const handleUserInput = function (key) {
-  console.log(key);
-  if (key === '\u0003') { return process.exit(); }
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-
-
-setupInput();
-
-
-
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
