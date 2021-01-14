825b005cbb89907c1054e012ee4d8b5d8cb4cad6
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-01-14 06:18:27 +0000

    adding wasd for directional keys

--- input.js
+++ input.js
@@ -1,16 +1,41 @@
+//stores the active TCP connection object
+//this connection is made global,
+//so it can be found by other functions
+
+let connection;
+
+//this one is standard input/output
+//to your server
+
 const handleUserInput = function (key) {
-  console.log(key);
   if (key === "\u0003") {
     return process.exit();
-  }
+  } else if (key === "\u0077") {
+    return connection.write("Move: up");
+  } else if (key === "\u0061") {
+    return connection.write("Move: left");
+  } else if (key === "\u0073") {
+    return connection.write("Move: down");
+  } else if (key === "\u0064") {
+    return connection.write("Move: right");
+  } else return undefined;
 };
 
-const setupInput = function () {
+//setupInput is passed connection() from play.js
+//set connections value, which changes
+//the global object we called connections
+//This funciton is for standard in/out
+//to your computer console
+//
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+
   return stdin;
 };
 
