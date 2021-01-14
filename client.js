30b19c58fe23cb7e3a414da06010d748fc0e3cdb
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-01-14 03:26:30 +0000

    take input from ctrl+c

--- client.js
+++ client.js
@@ -1,5 +1,28 @@
 const net = require("net");
 
+
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === '\u0003') { return process.exit(); }
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+
+
+setupInput();
+
+
+
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
@@ -14,21 +37,14 @@ const connect = function () {
   //Connection Successful
   conn.on("connect", () => {
     console.log("Connection Successful");
-    conn.write('Name: NIK')
-   
-    })
-    
-  
-  conn.on('connect', () => {
- //     setInterval(() => conn.write('Move: up'), 1000);
- //     setInterval(() => conn.write('Move: left'), 4000);
- //     setInterval(() => conn.write('Move: right'), 6000);
+    conn.write("Name: NIK");
+  });
 
-  
-    });
-
-
-
+  conn.on("connect", () => {
+    //     setInterval(() => conn.write('Move: up'), 1000);
+    //     setInterval(() => conn.write('Move: left'), 4000);
+    //     setInterval(() => conn.write('Move: right'), 6000);
+  });
 
   //data from server
   conn.on("data", (data) => {
